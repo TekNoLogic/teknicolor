c38ea38595d0a9651fa362e2daa4099ab3ab6c97
Tekkub Stoutwrithe <tekkub@gmail.com>
2006-12-20 05:17:40 +0000

    teknicolor - Tweaks and donglings
    git-svn-id: http://tekkub-wow.googlecode.com/svn/trunk/teknicolor@107 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- teknicolor.lua
+++ teknicolor.lua
@@ -1,45 +1,131 @@
-Teknicolor = {}
 
-local BC = AceLibrary("Babble-Class-2.2")
+local revclass = DongleStub("DongleUtils").classnamesreverse
+local tohex = DongleStub("DongleUtils").RGBPercToHex
 
-local names = {}
-Teknicolor.nametable = names
-local function register(name, class)
-	if not class or not name or names[name] then return end
-	local color = BC:GetHexColor(class)
-	if color == "a0a0a0" then return end
-	names[name] = "|cff"..color.."["..name.."]|r"
+
+local colors, x, names, names_mt = {}, {}, {}, {}
+setmetatable(names, names_mt)
+
+
+for class,eng in pairs(revclass) do
+	local c = RAID_CLASS_COLORS[string.upper(eng)]
+	local hex = tohex(c.r, c.g, c.b)
+	colors[eng] = hex
+	colors[class] = hex
+	colors[string.upper(eng)] = hex
 end
-local _, class = UnitClass("player")
-register(UnitName("player"), class)
 
-local _G = getfenv(0)
-for i=1,7 do
-	local f = _G["ChatFrame"..i]
-	local add = f.AddMessage
-	f.AddMessage = function(this,text,r,g,b,id)
-		local name = arg2
-		if event == "CHAT_MSG_SYSTEM" then _, _, name = string.find(text, "|h%[(.+)%]|h") end
-		if name and names[name] then text = string.gsub(text, "|h%["..name.."%]|h", "|h"..names[name].."|h") end
-		add(this,text,r,g,b,id)
+
+function names_mt.__newindex(t, k, v)
+	if not v or not k or x[k] then return end
+	local color = colors[v]
+	if not color then return end
+	x[k] = "|cff"..color.."["..k.."]|r"
+end
+
+
+function names_mt.__index(t, k)
+	return x[k]
+end
+
+
+
+teknicolor = Dongle:New("teknicolor")
+
+
+function teknicolor:Initialize()
+	local _G = getfenv(0)
+	for i=1,7 do
+		local f = _G["ChatFrame"..i]
+		local add = f.AddMessage
+		f.AddMessage = function(this, text, ...)
+			local name = arg2
+			if event == "CHAT_MSG_SYSTEM" then name = select(3, string.find(text, "|h%[(.+)%]|h")) end
+			if name and names[name] then text = string.gsub(text, "|h%["..name.."%]|h", "|h"..names[name].."|h") end
+			add(this, text, ...)
+		end
 	end
 end
 
-local eventfuncs = {
-	VARIABLES_LOADED = function() if IsInGuild() then GuildRoster() end if GetNumFriends() > 0 then ShowFriends() end end,
-	FRIENDLIST_UPDATE = function() for i=1,GetNumFriends() do local name, _, class = GetFriendInfo(i) register(name, class) end end,
-	GUILD_ROSTER_UPDATE = function() for i=1,GetNumGuildMembers(true) do local name, _, _, _,class = GetGuildRosterInfo(i) register(name, class) end end,
-	RAID_ROSTER_UPDATE = function() for i=1,GetNumRaidMembers() do local name, _, _, _, _, class = GetRaidRosterInfo(i) register(name, class) end end,
-	PARTY_MEMBERS_CHANGED = function() for i=1,GetNumPartyMembers() do local unit = "party" .. i local _, class = UnitClass(unit) register(UnitName(unit), class) end end,
-	PLAYER_TARGET_CHANGED = function() if not UnitIsPlayer("target") or not UnitIsFriend("player", "target") then return end local _, class = UnitClass("target") register(UnitName("target"), class) end,
-	WHO_LIST_UPDATE = function() for i=1,GetNumWhoResults() do local name, _, _, _, class = GetWhoInfo(i) register(name, class) end end,
-	UPDATE_MOUSEOVER_UNIT = function() if not UnitIsPlayer("mouseover") or not UnitIsFriend("player", "mouseover") then return end local _, class = UnitClass("mouseover") register(UnitName("mouseover"), class) end,
-	CHAT_MSG_SYSTEM = function() local _,_,name, class = string.find(arg1, "^|Hplayer:%w+|h%[(%w+)%]|h: Level %d+ %w+ (%w+) %- .+$") if name and class then register(name, class) end end,
-}
-local frame = CreateFrame("frame")
-frame.name = "Teknicolor"
-frame:Show()
-frame:SetScript("OnEvent", function() if eventfuncs[event] then eventfuncs[event]() end end)
-for e in pairs(eventfuncs) do frame:RegisterEvent(e) end
 
+function teknicolor:Enable()
+	register(UnitName("player"), select(2, UnitClass("player")))
+
+	self:RegisterEvent(VARIABLES_LOADED)
+	self:RegisterEvent(FRIENDLIST_UPDATE)
+	self:RegisterEvent(GUILD_ROSTER_UPDATE)
+	self:RegisterEvent(RAID_ROSTER_UPDATE)
+	self:RegisterEvent(PARTY_MEMBERS_CHANGED)
+	self:RegisterEvent(PLAYER_TARGET_CHANGED)
+	self:RegisterEvent(WHO_LIST_UPDATE)
+	self:RegisterEvent(UPDATE_MOUSEOVER_UNIT)
+	self:RegisterEvent(CHAT_MSG_SYSTEM)
+end
+
+
+function teknicolor:VARIABLES_LOADED()
+	if IsInGuild() then GuildRoster() end
+	if GetNumFriends() > 0 then ShowFriends() end
+end
+
+
+function teknicolor:FRIENDLIST_UPDATE()
+	for i=1,GetNumFriends() do
+		local name, _, class = GetFriendInfo(i)
+		register(name, class)
+	end
+end
+
+
+function teknicolor:GUILD_ROSTER_UPDATE()
+	for i=1,GetNumGuildMembers(true) do
+		local name, _, _, _,class = GetGuildRosterInfo(i)
+		register(name, class)
+	end
+end
+
+
+function teknicolor:RAID_ROSTER_UPDATE()
+	for i=1,GetNumRaidMembers() do
+		local name, _, _, _, _, class = GetRaidRosterInfo(i)
+		register(name, class)
+	end
+end
+
+
+function teknicolor:PARTY_MEMBERS_CHANGED()
+	for i=1,GetNumPartyMembers() do
+		local unit = "party".. i
+		local _, class = UnitClass(unit)
+		register(UnitName(unit), class)
+	end
+end
+
+
+function teknicolor:PLAYER_TARGET_CHANGED()
+	if not UnitIsPlayer("target") or not UnitIsFriend("player", "target") then return end
+	local _, class = UnitClass("target")
+	register(UnitName("target"), class)
+end
+
+
+function teknicolor:WHO_LIST_UPDATE()
+	for i=1,GetNumWhoResults() do
+		local name, _, _, _, class = GetWhoInfo(i)
+		register(name, class)
+	end
+end
+
+
+function teknicolor:UPDATE_MOUSEOVER_UNIT()
+	if not UnitIsPlayer("mouseover") or not UnitIsFriend("player", "mouseover") then return end
+	local _, class = UnitClass("mouseover")
+	register(UnitName("mouseover"), class)
+end
+
+
+function teknicolor:CHAT_MSG_SYSTEM()
+	local _, _, name, class = string.find(arg1, "^|Hplayer:%w+|h%[(%w+)%]|h: Level %d+ %w+ (%w+) %- .+$")
+	if name and class then register(name, class) end
+end
 
