27e44dc9c93e7799f5431b42ccfac0cf1f2bf226
Tekkub Stoutwrithe <tekkub@gmail.com>
2006-12-21 07:47:32 +0000

    teknicolor - Big tweaks and stuff
    git-svn-id: http://tekkub-wow.googlecode.com/svn/trunk/teknicolor@108 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- teknicolor.lua
+++ teknicolor.lua
@@ -1,39 +1,31 @@
 
 local revclass = DongleStub("DongleUtils").classnamesreverse
 local tohex = DongleStub("DongleUtils").RGBPercToHex
-
-
-local colors, x, names, names_mt = {}, {}, {}, {}
-setmetatable(names, names_mt)
-
-
-for class,eng in pairs(revclass) do
-	local c = RAID_CLASS_COLORS[string.upper(eng)]
-	local hex = tohex(c.r, c.g, c.b)
-	colors[eng] = hex
-	colors[class] = hex
-	colors[string.upper(eng)] = hex
-end
-
-
-function names_mt.__newindex(t, k, v)
-	if not v or not k or x[k] then return end
-	local color = colors[v]
-	if not color then return end
-	x[k] = "|cff"..color.."["..k.."]|r"
-end
-
-
-function names_mt.__index(t, k)
-	return x[k]
-end
-
+local colors, x, names = {}, {}, {}
+setmetatable(names, {
+	__index = function(t, k) return x[k] end,
+	__newindex = function(t, k, v)
+		teknicolor:Debug(1,k,v)
+		if not v or not k or x[k] or not colors[v] then return end
+		x[k] = string.format("|cff%s[%s]|r", colors[v], k)
+	end,
+})
 
 
 teknicolor = Dongle:New("teknicolor")
 
-
 function teknicolor:Initialize()
+	self:EnableDebug(1)
+
+	for class,eng in pairs(revclass) do
+		local c = RAID_CLASS_COLORS[string.upper(eng)]
+		local hex = tohex(c.r, c.g, c.b)
+		colors[eng] = hex
+		colors[class] = hex
+		colors[string.upper(eng)] = hex
+	end
+	revclass = nil
+
 	local _G = getfenv(0)
 	for i=1,7 do
 		local f = _G["ChatFrame"..i]
@@ -49,17 +41,17 @@ end
 
 
 function teknicolor:Enable()
-	register(UnitName("player"), select(2, UnitClass("player")))
+	names[UnitName("player")] = select(2, UnitClass("player"))
 
-	self:RegisterEvent(VARIABLES_LOADED)
-	self:RegisterEvent(FRIENDLIST_UPDATE)
-	self:RegisterEvent(GUILD_ROSTER_UPDATE)
-	self:RegisterEvent(RAID_ROSTER_UPDATE)
-	self:RegisterEvent(PARTY_MEMBERS_CHANGED)
-	self:RegisterEvent(PLAYER_TARGET_CHANGED)
-	self:RegisterEvent(WHO_LIST_UPDATE)
-	self:RegisterEvent(UPDATE_MOUSEOVER_UNIT)
-	self:RegisterEvent(CHAT_MSG_SYSTEM)
+	self:RegisterEvent("VARIABLES_LOADED")
+	self:RegisterEvent("FRIENDLIST_UPDATE")
+	self:RegisterEvent("GUILD_ROSTER_UPDATE")
+	self:RegisterEvent("RAID_ROSTER_UPDATE")
+	self:RegisterEvent("PARTY_MEMBERS_CHANGED")
+	self:RegisterEvent("PLAYER_TARGET_CHANGED")
+	self:RegisterEvent("WHO_LIST_UPDATE")
+	self:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
+	self:RegisterEvent("CHAT_MSG_SYSTEM")
 end
 
 
@@ -72,7 +64,7 @@ end
 function teknicolor:FRIENDLIST_UPDATE()
 	for i=1,GetNumFriends() do
 		local name, _, class = GetFriendInfo(i)
-		register(name, class)
+		names[name] = class
 	end
 end
 
@@ -80,7 +72,7 @@ end
 function teknicolor:GUILD_ROSTER_UPDATE()
 	for i=1,GetNumGuildMembers(true) do
 		local name, _, _, _,class = GetGuildRosterInfo(i)
-		register(name, class)
+		names[name] = class
 	end
 end
 
@@ -88,7 +80,7 @@ end
 function teknicolor:RAID_ROSTER_UPDATE()
 	for i=1,GetNumRaidMembers() do
 		local name, _, _, _, _, class = GetRaidRosterInfo(i)
-		register(name, class)
+		names[name] = class
 	end
 end
 
@@ -97,7 +89,7 @@ function teknicolor:PARTY_MEMBERS_CHANGED()
 	for i=1,GetNumPartyMembers() do
 		local unit = "party".. i
 		local _, class = UnitClass(unit)
-		register(UnitName(unit), class)
+		names[UnitName(unit)] = class
 	end
 end
 
@@ -105,14 +97,14 @@ end
 function teknicolor:PLAYER_TARGET_CHANGED()
 	if not UnitIsPlayer("target") or not UnitIsFriend("player", "target") then return end
 	local _, class = UnitClass("target")
-	register(UnitName("target"), class)
+	names[UnitName("target")] = class
 end
 
 
 function teknicolor:WHO_LIST_UPDATE()
 	for i=1,GetNumWhoResults() do
 		local name, _, _, _, class = GetWhoInfo(i)
-		register(name, class)
+		names[name] = class
 	end
 end
 
@@ -120,12 +112,12 @@ end
 function teknicolor:UPDATE_MOUSEOVER_UNIT()
 	if not UnitIsPlayer("mouseover") or not UnitIsFriend("player", "mouseover") then return end
 	local _, class = UnitClass("mouseover")
-	register(UnitName("mouseover"), class)
+	names[UnitName("mouseover")] = class
 end
 
 
 function teknicolor:CHAT_MSG_SYSTEM()
 	local _, _, name, class = string.find(arg1, "^|Hplayer:%w+|h%[(%w+)%]|h: Level %d+ %w+ (%w+) %- .+$")
-	if name and class then register(name, class) end
+	if name and class then names[name] = class end
 end
 
